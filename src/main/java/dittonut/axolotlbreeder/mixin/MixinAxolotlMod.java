c3f3113432d416c25139cebc7163d6dd0f21aeab
plz-Enter-name <104212145+plz-Enter-name@users.noreply.github.com>
2023-09-16 16:51:00 +0900

    Update MixinAxolotlMod.java

--- src/main/java/dittonut/axolotlbreeder/mixin/MixinAxolotlMod.java
+++ src/main/java/dittonut/axolotlbreeder/mixin/MixinAxolotlMod.java
@@ -1,7 +1,7 @@
 package dittonut.axolotlbreeder.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -15,15 +15,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AnimalEntity.class)
 public class MixinAxolotlMod extends Entity {
-    protected MixinAxolotlMod(EntityType<? extends AnimalEntity> entityType, World world) {
-        super(entityType, world);
-    }
-
     @Inject(method = "handleStatus", at = @At("RETURN"))
     private void onHandleStatus(byte status, CallbackInfo ci) {
         if (status == 18) {
             // the entity is breeding!!!!!
-
+            
         }
     }
 }
