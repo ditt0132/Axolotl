e24ca3e62c14a701d95cfae61e36054ab87195da
plz-Enter-name <javan.trash0123@gmail.com>
2023-09-17 10:12:46 +0900

    adsdd

--- src/main/java/dittonut/axolotlbreeder/mixin/MixinAxolotlMod.java
+++ src/main/java/dittonut/axolotlbreeder/mixin/MixinAxolotlMod.java
@@ -1,8 +1,11 @@
 package dittonut.axolotlbreeder.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,14 +28,22 @@ public abstract class MixinAxolotlMod extends PassiveEntity {
     private void onHandleStatus(byte status, CallbackInfo ci) {
         if (status == 18) {
             // the entity is breeding!!!!!
+            MinecraftClient.getInstance().player.sendMessage(Text.of("text at breed(not checked)"));
+            MinecraftClient.getInstance().player.sendMessage(Text.of(this.getType().toString()));
             if (this.getType().equals(EntityType.AXOLOTL)) {
-                Timer timer = new Timer();
+                // Timer timer = new Timer();
+                MinecraftClient.getInstance().player.sendMessage(Text.of("text at breed(only axolotl)"));
+                MixinAxolotlMod axolotl = this;
+                assert MinecraftClient.getInstance().player != null;
+                MinecraftClient.getInstance().player.sendMessage(Text.of("after setGlow"));
+                /*
                 timer.schedule(new TimerTask() {
                     @Override
                     public void run() {
-
+                        axolotl.setGlowing(true);
                     }
-                }, 300000); /// 300,000 ms = 5m
+                }, 3); /// 300,000 ms = 5m
+                */
             }
         }
     }
